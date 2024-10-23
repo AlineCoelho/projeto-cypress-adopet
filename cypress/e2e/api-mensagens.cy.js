63b03b0e326c7958847e870e77ba32f208986de5
Aline Coelho <alinedamasceno0810@gmail.com>
2024-10-23 15:02:12 -0300

    site adote seu pet em  cypress

---
+++ cypress/e2e/api-mensagens.cy.js
@@ -0,0 +1,16 @@
+describe('Api Adopet', () => {
+        
+    it('Mensagens da API', () => {
+        cy.request({
+            method: 'GET' ,
+            url: 'https://adopet-api-i8qu.onrender.com/mensagem/5fee2faa-535f-4cef-8643-cb3ce096f5f6',
+            headers: Cypress.env()
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('msg')
+            expect(res.duration).to.be.lte(tempoEsperado)
+
+    })
+  })
+})
